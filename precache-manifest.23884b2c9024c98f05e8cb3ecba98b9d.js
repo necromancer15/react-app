8a5ef520ef8d4eb3f2e57a4a15f441ca67b81aa5
necromancer15 <48450263+necromancer15@users.noreply.github.com>
2019-03-14 01:09:42 -0300

    Updates

---
+++ precache-manifest.23884b2c9024c98f05e8cb3ecba98b9d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb60cb7376e75b90ae7c6ef0372fe52",
+    "url": "/react-app/static/media/kazuma.5bb60cb7.jpg"
+  },
+  {
+    "revision": "39fe7323b690fa2c6419",
+    "url": "/react-app/static/css/main.79b0db6b.chunk.css"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "b92d86b38a4d1d1159b565a8f5a101c0",
+    "url": "/react-app/static/media/tanya.b92d86b3.jpg"
+  },
+  {
+    "revision": "9548385644a302f071d3",
+    "url": "/react-app/static/js/2.0e9fd98a.chunk.js"
+  },
+  {
+    "revision": "de81acff0b83c38a8210cd819d42ca1d",
+    "url": "/react-app/static/media/ainz.de81acff.jpg"
+  },
+  {
+    "revision": "3285df2c7a9513d10d8ac79a57dd8769",
+    "url": "/react-app/static/media/elias.3285df2c.jpg"
+  },
+  {
+    "revision": "00fb72d62381a57ef95e97d412b2b916",
+    "url": "/react-app/static/media/esdeath.00fb72d6.jpg"
+  },
+  {
+    "revision": "ba36804581494f3e25eae5f12df1889f",
+    "url": "/react-app/static/media/glenn.ba368045.jpg"
+  },
+  {
+    "revision": "c529adfb941863859db829b786fbb6d8",
+    "url": "/react-app/static/media/gslayer.c529adfb.jpg"
+  },
+  {
+    "revision": "60006d48cf380c407ff754fe6587103c",
+    "url": "/react-app/static/media/iori.60006d48.jpg"
+  },
+  {
+    "revision": "39fe7323b690fa2c6419",
+    "url": "/react-app/static/js/main.80ed4dbc.chunk.js"
+  },
+  {
+    "revision": "89c9491b9f2899ba2ac83a32186065c2",
+    "url": "/react-app/static/media/madara.89c9491b.jpg"
+  },
+  {
+    "revision": "8c8d925d3150eea4806f8b36cc5eee1f",
+    "url": "/react-app/static/media/megumin.8c8d925d.jpg"
+  },
+  {
+    "revision": "a6a9241ad36cb8ef35eaa4f2c32b1921",
+    "url": "/react-app/static/media/mikasa.a6a9241a.jpg"
+  },
+  {
+    "revision": "5d9f94041fe59888411df742da91f6ad",
+    "url": "/react-app/static/media/milim.5d9f9404.jpg"
+  },
+  {
+    "revision": "82ffe5b8f7be88115bb292c99b25f357",
+    "url": "/react-app/static/media/rem.82ffe5b8.jpg"
+  },
+  {
+    "revision": "fd51db74fdd408bacea58e719c4f47d4",
+    "url": "/react-app/static/media/rory.fd51db74.jpg"
+  },
+  {
+    "revision": "69dfb04174e3d2d5c0d3743a5cbb16dc",
+    "url": "/react-app/static/media/satan.69dfb041.jpg"
+  },
+  {
+    "revision": "74b25ee96a44c64f344fc498f518d766",
+    "url": "/react-app/static/media/shalltear.74b25ee9.jpg"
+  },
+  {
+    "revision": "9548385644a302f071d3",
+    "url": "/react-app/static/css/2.c4bbc8fd.chunk.css"
+  },
+  {
+    "revision": "b028e9857dbebc40a4eaac2f6366f028",
+    "url": "/react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efd7f174d3968e784d510270fcc0e164.js"
+  "/react-app/precache-manifest.23884b2c9024c98f05e8cb3ecba98b9d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
